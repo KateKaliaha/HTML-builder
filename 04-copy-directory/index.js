80d302c3de778fbfcd0e1b996506e799faae3c1a
Katherina Kaliaha <mellinda077@mail.ru>
2022-05-21 13:14:57 +0300

    complete task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
+const fs = require('fs');
+const path = require ('path');
 
+
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+  if (err) throw err;
+});
+fs.readdir(path.join(__dirname, 'files-copy'), (err, files) => {
+  if (err) throw err;
+  files.forEach(file => {
+    fs.unlink(path.join(__dirname, 'files-copy', file),  (err) => {
+      if (err) throw err;
+    });
+  });
+});
+      
+async function copyDir () {
+  await fs.readdir(path.join(__dirname, 'files'), {withFileTypes: true}, (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    data.forEach(file => {
+      fs.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name), (err) => {
+        if (err) throw err;         
+      });
+    });
+       
+  });
+}
+
+copyDir();
